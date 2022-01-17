fab7bb9ac9cebe05ff61fe8ef4a46dc506becd1f
kishor <kishorbhandari608@gmail.com>
2022-01-17 13:36:28 +0530

    nav__responsive_mobile

--- src/components/Header/index.jsx
+++ src/components/Header/index.jsx
@@ -10,21 +10,29 @@ import {
   IconButton,
   Badge,
   Button,
+  Backdrop,
+  Modal,
+  Fade,
   Divider,
   Typography,
+  MenuItem,
+  Menu,
+  Box,
 } from "@material-ui/core"
 import {
-  Mail,
   Facebook,
   Instagram,
   Twitter,
   Settings,
   ContactSupportTwoTone,
+  FeedbackOutlined,
 } from "@material-ui/icons"
 
 export const Header = () => {
   // @section constant
   let date = new Date().toDateString()
+  const [anchorEl, setAnchorEl] = React.useState(null)
+  const open = Boolean(anchorEl)
 
   // @section Functions
   function notificationsLabel(count) {
@@ -36,41 +44,78 @@ export const Header = () => {
     }
     return `${count} notifications`
   }
+
+  const handleClick = (event) => {
+    setAnchorEl(event.currentTarget)
+  }
+  const handleCl = () => {
+    setAnchorEl(null)
+  }
+
   return (
     <>
       <Helmet>
         <title>Hash Technologies | Employee Panel</title>
       </Helmet>
+      <nav>
+        <div className="Nav__container">
+          <img className="Nav__container__logo" src={logo12} alt="logo" />
 
-      <div className="Nav__container">
-        <img className="Nav__container__logo" src={logo12} alt="logo" />
+          <div className="Nav__contianer__right">
+            <Typography className="date">{date}</Typography>
 
-        <div className="Nav__contianer__right">
-          <Typography>{date}</Typography>
+            <ul className="social__icons">
+              <li className="social__icons__icons__wrap">
+                <ContactSupportTwoTone fontSize="small" />
+                <p className="icon__support__desciption">support</p>
+              </li>
 
-          <ul className="social__icons">
-            <li>
-              <ContactSupportTwoTone />
-            </li>
-            <li>
-              <Settings fontSize="small" />
-            </li>
-            <li>
-              <Facebook fontSize="small" />
-            </li>
-            <li>
-              <Instagram fontSize="small" />
-            </li>
-            <li>
-              <Twitter fontSize="small" />
-            </li>
-          </ul>
-          <img
-            className="Nav__container__right__logo__profile"
-            src="https://scontent.fbdp1-1.fna.fbcdn.net/v/t1.6435-9/86366870_2745388305514352_2727862943780700160_n.jpg?_nc_cat=103&ccb=1-5&_nc_sid=09cbfe&_nc_ohc=MkgCEbCIJGkAX_AkJ2H&_nc_ht=scontent.fbdp1-1.fna&oh=00_AT8s1LbVDqJD0Pm2yasSKWWx-_cGypx0m0kSJLxfPR3MsQ&oe=62049281"
-          />
+              <li className="social__icons__icons__wrap">
+                <FeedbackOutlined fontSize="small" />
+                <p className="icon__support__desciption">feedback</p>
+              </li>
+              <li className="social__icons__icons__wrap">
+                <Settings
+                  fontSize="small"
+                  id="basic-button"
+                  aria-controls={open ? "basic-menu" : undefined}
+                  aria-haspopup="true"
+                  aria-expanded={open ? "true" : undefined}
+                  onClick={handleClick}
+                />
+                <Menu
+                  id="basic-menu"
+                  anchorEl={anchorEl}
+                  open={open}
+                  onClose={handleCl}
+                  MenuListProps={{
+                    "aria-labelledby": "basic-button",
+                  }}
+                  style={{ marginTop: "40px" }}
+                >
+                  <MenuItem onClick={handleCl}>My account</MenuItem>
+                  <MenuItem onClick={handleCl}>Change password</MenuItem>
+                  <MenuItem onClick={handleCl}>Logout</MenuItem>
+                </Menu>
+                <p className="icon__support__desciption">settings</p>
+              </li>
+              <li className="social__icons__icons__wrap">
+                <Facebook fontSize="small" />
+              </li>
+              <li className="social__icons__icons__wrap">
+                <Instagram fontSize="small" />
+              </li>
+              <li className="social__icons__icons__wrap">
+                <Twitter fontSize="small" />
+              </li>
+            </ul>
+            <img
+              className="Nav__container__right__logo__profile"
+              src="https://scontent.fbdp1-1.fna.fbcdn.net/v/t1.6435-9/86366870_2745388305514352_2727862943780700160_n.jpg?_nc_cat=103&ccb=1-5&_nc_sid=09cbfe&_nc_ohc=MkgCEbCIJGkAX_AkJ2H&_nc_ht=scontent.fbdp1-1.fna&oh=00_AT8s1LbVDqJD0Pm2yasSKWWx-_cGypx0m0kSJLxfPR3MsQ&oe=62049281"
+            />
+          </div>
         </div>
-      </div>
+      </nav>
       <Divider variant="middle" style={{ marginTop: "10px" }} />
     </>
   )
--- src/screens/Home/index.js
+++ src/screens/Home/index.js
@@ -3,8 +3,19 @@ import React, { useState } from "react"
 import "./style.css"
 
 // @section material-ui
-import { Button } from "@material-ui/core"
-import { Mail, Phone } from "@material-ui/icons"
+import {
+  Button,
+  AppBar,
+  Box,
+  Toolbar,
+  IconButton,
+  Typography,
+  BottomNavigation,
+  BottomNavigationAction,
+  MenuItem,
+  Menu,
+} from "@material-ui/core"
+import { Mail, Phone, People, Book, ViewCompact } from "@material-ui/icons"
 
 import { Header } from "../../components/Header"
 import { Footer } from "../../components/Footer"
@@ -12,6 +23,8 @@ import { Footer } from "../../components/Footer"
 export const Home = () => {
   let time = new Date().toLocaleTimeString()
   const [ctime, setCtime] = useState(time)
+  const [user, setUser] = useState("employee")
+  const [value, setValue] = useState()
 
   const updateTime = () => {
     time = new Date().toLocaleTimeString()
@@ -19,6 +32,16 @@ export const Home = () => {
   }
 
   setInterval(updateTime, 1000)
+
+  if (user === "admin") {
+    return (
+      <>
+        <Header />
+        <h3>Admin section</h3>
+        <Footer />
+      </>
+    )
+  }
 
   return (
     <>
